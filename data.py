84465ac6a51f73fa32bf6aceb4a62ca08c820467
Chenda Zhang <chenda1997@hotmail.com>
2021-12-06 17:42:22 -0500

    lyrics

--- data.py
+++ data.py
@@ -3,12 +3,14 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+import pickle
 
 @st.cache(allow_output_mutation=True)
 def load_genre_audio():
 
     global exploded_track_df
     df = pd.read_csv("data/filtered_track_df.csv")
+    df["lyric_length"] = [len(item.split(" ")) for item in df.lyrics]
     df['genres'] = df.genres.apply(lambda x: [i[1:-1] for i in str(x)[1:-1].split(", ")])
     exploded_track_df = df.explode("genres")
 
@@ -45,6 +47,18 @@ def load_track_artist_album():
     global track_artist_album_df
     track_artist_album_df = pd.read_csv("data/trackArtistAlbum.csv")
 
+def load_lyrics_data():
+    global lyric_knn #the 10 nearest neighbor of each song using sentence embedding
+    global cleaned_lyric #The lyrics of songs in each genre with stopwords removed, used for word clouds
+    global lyric_sentiments # The sentiment distribution of each genre and 10 sentence examples for positive and negative each
+    global song_sentiments # The sentiment score for each song's each sentence
+    lyric_knn = pickle.load(open("data/lyrics_10nn.pkl",'rb'))
+    cleaned_lyric = pickle.load(open("data/cleaned_lyrics.pkl",'rb'))
+    lyric_sentiments = pickle.load(open("data/lyric_sentiments.pkl", 'rb'))
+    song_sentiments = pickle.load(open("data/song_sentiments.pkl",'rb'))
+
+
+
 @st.cache(allow_output_mutation=True)
 def load_data():
     data_dir = "data/SpotGenTrack/Data Sources/"
@@ -58,3 +72,4 @@ def load_data():
 
     load_genre_audio()
     load_track_artist_album()
+    load_lyrics_data()
